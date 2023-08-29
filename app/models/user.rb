c2f60f7eeda88b606d293c771294de79328b4fcc
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-29 12:08:48 +0900

    devise導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  columns = [
+    :name,
+    :email,
+    :encrypted_password,
+    :last_name_ch,
+    :first_name_ch,
+    :last_name,
+    :first_name,
+    :date_of_birth
+  ]
+  columns.each do |c|
+    validates c, presence: true
+  end
 end
