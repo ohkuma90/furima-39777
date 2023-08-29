25033d73aa680e6e4146dc6ad8b7adf548fe6282
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-29 14:24:23 +0900

    単体テストコード記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,10 +4,10 @@ FactoryBot.define do
     email {Faker::Internet.email}
     password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name_ch{}
-    first_name_ch{}
-    last_name{}
-    first_name{}
-    date_of_birth{}
+    last_name_ch{'佐藤'}
+    first_name_ch{'次郎'}
+    last_name{'サトウ'}
+    first_name{'ジロウ'}
+    date_of_birth{Faker::Date.birthday(min_age: 18, max_age: 65)}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
 
   describe 'ユーザー新規登録' do
     context '新規登録できる場合' do
-      it "nameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+      it "nameとemail、passwordとpassword_confirmationとlast_name_ch、first_name_ch、last_name、first_nameとdate_of_birthが存在すれば登録できる" do
         expect(@user).to be_valid
       end
     end
@@ -59,6 +59,36 @@ RSpec.describe User, type: :model do
         @user.email = 'testmail'
         @user.valid?
         expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+
+      it "last_name_chが空では登録できない" do
+        @user.last_name_ch = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name ch can't be blank")
+      end
+
+      it "first_name_chが空では登録できない" do
+        @user.first_name_ch = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name ch can't be blank")
+      end
+
+      it "last_nameが空では登録できない" do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank")
+      end
+
+      it "first_nameが空では登録できない" do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank")
+      end
+
+      it "date_of_birthが空では登録できない" do
+        @user.date_of_birth = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Date of birth can't be blank")
       end
 
     end
