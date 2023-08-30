1b187bb886a026b1ccc3c9337c3d89baa692b1a0
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-30 21:47:32 +0900

    テストコード途中

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :days_to_ship_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true, message: "Price is out of setting range"}
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true, message: " is out of setting range"}
   validates :user, presence: true
   
   extend ActiveHash::Associations::ActiveRecordExtensions
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品できる場合' do
+      it 'nameとexplanationとcategory_idとcondition_idとdelivery_charge_idとprefecture_idとdays_to_ship_idとpriceとuserが存在していれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない場合' do
+      it 'imageが空では保存できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'nameが空では保存できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+    end
+  end
 end
