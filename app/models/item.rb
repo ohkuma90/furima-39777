f8ce2a6f6c36dd3bd0fcff3a6955dda6caeab3bd
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-30 16:36:11 +0900

    ビューのプルダウン反映

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :delivery_charge_id, numericality: { other_than: 1 } 
+  validates :prefecture_id, numericality: { other_than: 1 } 
+  validates :days_to_ship_id, numericality: { other_than: 1 } 
+
 end
 
