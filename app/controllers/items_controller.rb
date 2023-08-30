e1192f47e6c8f5dd69cc6c3fe641a2f50c7f8785
ohkuma90 <s.ohkuma001@gmail.com>
2023-08-30 16:48:00 +0900

    ストロングパラメーターの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,13 @@ class ItemsController < ApplicationController
     @items = Item.new
   end
 
-  
+  def create
+    Item.create(item_params)
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explanation, :category_id, :condition_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
 
 end
