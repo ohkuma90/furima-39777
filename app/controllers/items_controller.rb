35f188d95c813250f507784615e67845683abd63
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-01 17:10:05 +0900

    コントローラーの追記

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,7 +24,22 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+
+    if user_signed_in? || current_user != @item.user
+      redirect_to root_path
+    end
   end
+
+  def update
+    @item = Item.find(params[:id]) 
+
+  if @item.update(item_params)
+    redirect_to item_path(@item)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+  end
+
   private
 
   def item_params
