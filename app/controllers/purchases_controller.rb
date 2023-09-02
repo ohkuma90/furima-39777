db09da43d4f6678857ba5c45f517b270bb5f74ab
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-02 21:13:59 +0900

    コントローラーの記述・フォームの連結

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,13 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
-    @purchase = PurchaseForm.new
+    @purchase_form = PurchasesForm.new
   end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
--- app/models/purchases_form.rb
+++ app/models/purchases_form.rb
@@ -1,4 +1,4 @@
-class PurchaseForm
+class PurchasesForm
   include ActiveModel::Model
 
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :purchase_id
@@ -13,6 +13,6 @@ class PurchaseForm
 
   def save
     purchase = Purchase.create(item_id: @item.id, user_id: current_user.id)
-    Address.create(:post_code post_code, :prefecture_id prefecture_id, :municipalities municipalities, :street_address street_address, :building_name building_name, :telephone_number telephone_number, purchase_id: purchase.id)
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street_address: street_address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
