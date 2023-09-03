87e911ae6e1ffee83cbab93cce131f23925ab7a3
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-03 15:59:47 +0900

    データの保存実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -5,9 +5,25 @@ class PurchasesController < ApplicationController
     @purchase_form = PurchasesForm.new
   end
 
+  def create
+    @purchase_form = PurchasesForm.new(purchase_params)
+    if @purchase_form.valid?
+      @purchase_form.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+
   private
 
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def purchase_params
+    params.require(:purchases_form).permit(:post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number).merge(item_id: params[:item_id], user_id: current_user.id, )
+  end
+
 end
--- app/models/purchases_form.rb
+++ app/models/purchases_form.rb
@@ -8,11 +8,11 @@ class PurchasesForm
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :municipalities, presence: true
   validates :street_address, presence: true
-  validates :building_name, presence: true
   validates :telephone_number, presence: true
 
   def save
-    purchase = Purchase.create(item_id: @item.id, user_id: current_user.id)
+    binding.pry
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, municipalities: municipalities, street_address: street_address, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end
