93d47a8a03f4720fa67a58dd452501ce1fbbd552
ohkuma90 <s.ohkuma001@gmail.com>
2023-09-03 18:18:42 +0900

    テストコードの記述

--- app/models/purchases_form.rb
+++ app/models/purchases_form.rb
@@ -4,11 +4,11 @@ class PurchasesForm
   attr_accessor :item_id, :user_id, :post_code, :prefecture_id, :municipalities, :street_address, :building_name, :telephone_number, :purchase_id
   validates :item_id, presence: true
   validates :user_id, presence: true
-  validates :post_code, presence: true
+  validates :post_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :municipalities, presence: true
   validates :street_address, presence: true
-  validates :telephone_number, presence: true
+  validates :telephone_number, presence: true, numericality: { only_integer: true, message: "is invalid. Input only number" }, length: { minimum: 10, maximum: 11, }
 
   def save
     binding.pry
---
+++ spec/factories/purchases_form.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :purchases_form do
+    item_id { 1 }
+    user_id { 1 }
+    post_code { '000-0000' }
+    prefecture_id { 23 }
+    municipalities { '田町' }
+    street_address { '4-4-4' }
+    building_name { '田町ハイツ' }
+    telephone_number { '0000000000' }
+  end
+end
